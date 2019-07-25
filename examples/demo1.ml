0e85c4da364a1ba0e32f75f9cea22b9570b6a80a
kakadu <kakadu.hafanana@gmail.com>
2019-07-26 00:32:00 +0300

    WIP

--- examples/demo1.ml
+++ examples/demo1.ml
@@ -1,13 +1,13 @@
 let x = ref 0
-let rec f n = 
+let rec f n =
   if n<=0 then ()
   else incr x
 
-let g n = 
+(* let g n =
   f n;
   !x
 
-let h () = 
-  let n = Random.int 0 in 
+let h () =
+  let n = Random.int 0 in
   x := 42;
-  g n
+  g n *)
--- heap.ml
+++ heap.ml
@@ -12,7 +12,7 @@ module Longident = struct
   type t = [%import: Longident.t] [@@deriving sexp_of]
 end
 
-type term = LI of (t[@sexp.opaque]) * Longident.t
+type term = LI of (t [@sexp.opaque]) * Longident.t
           | CInt of int
           | BinOp of op * term * term
           | Unit
@@ -20,9 +20,9 @@ type term = LI of (t[@sexp.opaque]) * Longident.t
           | Union of (pf * term) list
           | Lambda of { lam_argname: Longident.t option
                       (* ; lam_env: t *)
-                      ; lam_body: term 
+                      ; lam_body: term
                       }
-and pf  = LogicBinOp of logic_op * pf * pf 
+and pf  = LogicBinOp of logic_op * pf * pf
         | Not of pf
         | Term of term
 [@@deriving sexp_of]
@@ -32,8 +32,8 @@ and t = HAssoc of (Longident.t * term) list
         (* Heap should be a mapping from terms to terms (array access, for example)
          * but for fibonacci it doesn't matter
          *)
-      | HMerge of (pf * t) list 
-      | HCmps of t * t 
+      | HMerge of (pf * t) list
+      | HCmps of t * t
       | HEmpty
 [@@deriving sexp_of]
 
@@ -46,14 +46,14 @@ let union2 g1 t1 g2 t2 = union [(g1,t1); (g2,t2)]
 let binop op a b = BinOp (op,a,b)
 
 (** Propositonal formula operations *)
-let pf_term el = Term el 
+let pf_term el = Term el
 let pf_not pf = Not pf
 let pf_binop op f1 f2 = LogicBinOp (op, f1, f2)
 
 
 (** Heap operations *)
-let cmps : t -> t -> t = fun a b -> 
-  match (a,b) with 
+let cmps : t -> t -> t = fun a b ->
+  match (a,b) with
   | (HEmpty,_) -> b
   | (_,HEmpty) -> a
   | (HAssoc xs, HAssoc ys) -> HAssoc (xs @ ys)
@@ -63,10 +63,10 @@ let single name el : t = HAssoc [(name,el)]
 let merge2 g1 h1 g2 h2 = HMerge [(g1,h1); (g2,h2)]
 
 (** The main heap access function *)
-let hfind_exn ident heap = 
-  match heap with 
+let hfind_exn ident heap =
+  match heap with
   | _ -> raise Not_found
 
-let hfind_li longident heap = 
-  try hfind_exn longident heap 
+let hfind_li longident heap =
+  try hfind_exn longident heap
   with Not_found -> li longident
