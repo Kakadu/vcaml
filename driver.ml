c9c9981f6c5f4de83157c376cc13c22be56f5c57
kakadu <kakadu.hafanana@gmail.com>
2019-07-30 01:35:41 +0300

    WIP on idents and paths

--- driver.ml
+++ driver.ml
@@ -18,140 +18,145 @@ let is_arr expr =
 
 let i_to_longi i = Longident.Lident (Ident.unique_name i)
 
-let standart_heap = Heap.empty
+let standart_heap = Heap.hempty
   (* Heap.single (Lident "!") (Heap.lambda (Some (Lident "x")) Heap.empty (Heap.li (Heap.empty) (Lident "x"))) *)
 
-let is_binop = function 
-  | "<=" | "<" | ">" | ">=" | "+" | "-"  -> true 
-  | _ -> false 
+let is_binop = function
+  | "<=" | "<" | ">" | ">=" | "+" | "-"  -> true
+  | _ -> false
 
-let find_ident api heap lident = 
-  Heap.hfind_li heap lident
+let find_lident api heap lident =
+  match Heap.Api.is_pending_lident api lident with
+  | Some myident -> Heap.li ~heap myident
+  | None ->
+      match Heap.Api.find_lident_exn api lident with
+      | term -> term
+      | exception Not_found -> assert false
 
 let rec process_str api { str_items; _ } : Heap.Api.t * Heap.t =
-  List.fold_left ~f:process_si ~init:(api,Heap.empty) str_items
+  List.fold_left ~f:process_si ~init:(api, Heap.hempty) str_items
 and process_si (api,heap) { str_desc; _} : Heap.Api.t * Heap.t =
-  match str_desc with 
-| Tstr_value (recflg, [vb]) -> begin 
-    match process_vb (api,heap) vb with 
-    | (Some lident, ans, h) -> 
-        ( Heap.Api.add api lident ans 
-        , Heap.cmps heap h)
-    | _ -> assert false 
+  match str_desc with
+| Tstr_value (recflg, [vb]) -> begin
+    match process_vb (api,heap) recflg vb with
+    | (Some ident, ans, h) ->
+        ( Heap.Api.add api ident ans
+        , Heap.hcmps heap h)
+    | _ -> assert false
   end
 | Tstr_value (recflg, vbs) -> assert false
 | _ -> assert false
-and process_vb (api,heap) _recflg { vb_pat; vb_expr; _ } : Longident.t option * Heap.term * Heap.t =
-  (* returns maybe identifier, 
-   * rhs effect, 
+and process_vb (api,heap) _recflg { vb_pat; vb_expr; _ } : Heap.MyIdent.t option * Heap.term * Heap.t =
+  (* returns maybe identifier,
+   * rhs effect,
    * effect thats is created by binding creation *)
   (* let api1 = Api.add_pending api *)
   match vb_pat.pat_desc with
   | Tpat_var (ident,_) ->
       let (_new_api,ans,h) = process_expr (api,heap) vb_expr in
-      (Some (i_to_longi ident), h, ans)
+      (Some ident, h, ans)
   | _ -> failwith "not implemented"
 
-(** Accepts 
+(** Accepts
  * 1) Current state of API
  * 2) current accumulated effect
  * 3) expression to process
- * Returns 
+ * Returns
  * 1) new API
- * 2) accumulated effect combined with previous one 
+ * 2) accumulated effect combined with previous one
  * 3) term result
  *)
 and process_expr (api,heap) e =
   match e.exp_desc with
-  | Texp_constant (Asttypes.Const_int n) -> (api, Heap.empty, Heap.CInt n)
-  | Texp_construct ({txt=Lident "()"},_,[]) -> (api, Heap.empty, Heap.Unit)
-  | Texp_ident (_,{txt=ident},_) ->
+  | Texp_constant (Asttypes.Const_int n) -> (api, Heap.hempty, Heap.CInt n)
+  | Texp_construct ({txt=Lident "()"},_,[]) -> (api, Heap.hempty, Heap.Unit)
+  | Texp_ident (_,{txt=lident},_) ->
+    (* TODO: use path here *)
     (* identifiers are returned as is. No inlining yet, even for functions *)
-    (api, Heap.empty, find_ident api heap ident)
+    (api, Heap.hempty, find_lident api heap lident)
   | Texp_function { cases=[{c_guard=None; c_lhs={pat_desc=Tpat_construct({txt=Lident "()"},_,[])}; c_rhs}] } ->
         (* Processing `fun () -> c_rhs` *)
-    let api, h, ans = process_expr (api,heap) c_rhs in 
-    (api, Heap.empty, Heap.lambda None api h ans)
+    let api, h, ans = process_expr (api,heap) c_rhs in
+    (api, Heap.hempty, Heap.lambda false None (fst api) h ans)
 
   | Texp_function { cases=[{c_guard=None; c_lhs={pat_desc=Tpat_var(argname,_)}; c_rhs}] } ->
         (* Processing `fun argname -> c_rhs` *)
-    let api, h, ans = process_expr (api,heap) c_rhs in 
-    (api, Heap.empty, Heap.lambda (Some (i_to_longi argname)) api h ans)
+    let api, h, ans = process_expr (api,heap) c_rhs in
+    (api, Heap.hempty, Heap.lambda false (Some argname) (fst api) h ans)
 
   | Texp_let (_recflg, [vb], body) -> begin
       match process_vb (api,heap) _recflg vb with
       | (Some ident, rhs, heff) ->
           (* we don't care about isolation here, so we compose heaps with toplevel one *)
-          let heff2 = Heap.cmps heap heff in 
+          let heff2 = Heap.hcmps heap heff in
           (* we need to extend API before processing the body *)
-          let api1 = Api.add api ident rhs in
-          let api2, heff3, ans = process_expr (api1,heff2) body in
+          let api = Heap.Api.add api ident rhs in
+          let api, heff3, ans = process_expr (api,heff2) body in
           (api, heff3, ans)
-      | _ -> assert false 
+      | _ -> assert false
   end
   | Texp_let (_recflg, _vbs, _body) -> assert false
   | Texp_apply ({exp_desc=Texp_ident(_,{txt=Lident "ref"},_)}, [(_,Some e)])
   | Texp_apply ({exp_desc=Texp_ident(_,{txt=Ldot(Lident _, "ref")},_)}, [(_,Some e)]) ->
       (* Do we need explicit derefenrecing? *)
       process_expr (api, heap) e
-  | Texp_apply ({exp_desc=Texp_ident(_,{txt=Lident opname},_)}, [(_,Some l); (_,Some r) ]) 
+  | Texp_apply ({exp_desc=Texp_ident(_,{txt=Lident opname},_)}, [(_,Some l); (_,Some r) ])
         when is_binop opname -> begin
     (* binop *)
-    let op = match opname with 
+    let op = match opname with
       | "<=" -> Heap.LE
       | "<"  -> Heap.LT
-      | ">"  -> Heap.GT 
+      | ">"  -> Heap.GT
       | "+"  -> Heap.Plus
-      | _ -> failwith "not supported (weakly typed code)" 
-    in 
-    (* Although we don't need to return updated API we return it 
+      | _ -> failwith "not supported (weakly typed code)"
+    in
+    (* Although we don't need to return updated API we return it
      * to have a global cache of function summaries *)
-    let api_1,h1,l2 = process_expr (api,heap) l in 
-    let api_2,h2,r2 = process_expr (api_1,h1) r in 
+    let api_1,h1,l2 = process_expr (api,heap) l in
+    let api_2,h2,r2 = process_expr (api_1,h1) r in
     (api_2, h2, Heap.binop op l2 r2)
   end
-  | Texp_apply ({exp_desc=Texp_ident(_,{txt=Lident ":="},_)}, [(_,Some {exp_desc=Texp_ident(_,{txt=ident},_)}); (_,Some r) ]) ->
-    let api_1,h1,r1 = process_expr (api,heap) r in 
-    let heap_ans = Heap.cmps h1 (Heap.single ident r1) in 
+  | Texp_apply ({exp_desc=Texp_ident(_,{txt=Lident ":="},_)}, [(_,Some {exp_desc=Texp_ident(_,{txt=lident},_)}); (_,Some r) ]) ->
+    let api_1,h1,r1 = process_expr (api,heap) r in
+    let heap_ans = Heap.hcmps h1 (Heap.hsingle (Heap.Api.get_ident api lident) r1) in
     (* The idea is to have all optimisation operations in Heap module *)
     (api_1, heap_ans, Heap.Unit)
 
-  | Texp_apply ({exp_desc=Texp_ident(_,{txt=ident},_)}, [(_,Some e)]) -> begin 
+  | Texp_apply ({exp_desc=Texp_ident(_,{txt=lident},_)}, [(_,Some e)]) -> begin
     (* Now: real function application *)
-    let api,argeff,ans = process_expr (api,heap) e in    
+    let api,argeff,ans = process_expr (api,heap) e in
     (* now we need to compose effect of e with effect of call
        But for recursive calls -- we shouldn't
      *)
-    match find_ident api heap ident with 
-    | Heap.Lambda {lam_argname; lam_eff; lam_api; lam_body} -> 
-        (api, argeff, Heap.call (Heap.li heap ident) ans)
-    | Heap.LI (h, ident) as func -> (argeff, Heap.call func ans) 
-    | exception Not_found -> failwith (Printf.sprintf "Identifier unbound: '%s'" (Heap.Longident.to_string ident))
-    | ans_term -> 
-        failwith (sprintf "typecheck error? should not happed. Searched for ident %a. Got '%a'" 
-                    Heap.Longident.pp_string ident
+    match find_lident api heap lident with
+    | Heap.Lambda {lam_argname; lam_eff; lam_api; lam_body} ->
+        let lam_ident = Heap.Api.get_ident api lident in
+        (api, argeff, Heap.call (Heap.li ~heap lam_ident) ans)
+    | Heap.LI (h, ident) as func -> (api, argeff, Heap.call func ans)
+    | exception Not_found -> failwith (Printf.sprintf "Identifier unbound: '%a'" Heap.pp_longident lident)
+    | ans_term ->
+        failwith (sprintf "typecheck error? should not happed. Searched for ident %a. Got '%a'"
+                    Heap.pp_longident lident
                     Heap.pp_term ans_term
                 )
   end
   | Texp_sequence (a,b) ->
-    let effa,___ = process_expr heap a in
-    let effb,ans = process_expr heap b in
-    (Heap.cmps effa effb, ans)
-  | Texp_ifthenelse (econd,ethen,Some eelse) -> 
-    let (h1, e) = process_expr heap econd in
-    let h_after_cond = h1 in 
-    let (h2,th) = process_expr h_after_cond   ethen in 
-    let (h3,el) = process_expr h_after_cond   eelse in 
-    let g    = Heap.pf_term e in 
-    let notg = Heap.pf_not g in 
-    (Heap.merge2     g h2
-                  notg h3
-    , Heap.union2 g th notg el)
-  | Texp_match (what, cases, _exc_cases, _) -> begin 
-    match cases with 
-    | [{c_lhs={pat_desc=Tpat_construct ({txt=Lident "()"},_,[])}; c_rhs}] -> process_expr heap c_rhs
-    | _ -> assert false 
-  end 
+    let api,effa,___ = process_expr (api,heap) a in
+    let api,effb,ans = process_expr (api,heap) b in
+    (api, Heap.hcmps effa effb, ans)
+  | Texp_ifthenelse (econd,ethen,Some eelse) ->
+    let (api,h1, e) = process_expr (api,heap) econd in
+    let h_after_cond = h1 in
+    let (api,h2,th) = process_expr (api,h_after_cond) ethen in
+    let (api,h3,el) = process_expr (api,h_after_cond) eelse in
+    let g    = Heap.pf_term e in
+    let notg = Heap.pf_not g in
+    (api, Heap.hmerge2     g h2 notg h3, Heap.union2 g th notg el)
+  | Texp_match (what, cases, _exc_cases, _) -> begin
+    match cases with
+    | [{c_lhs={pat_desc=Tpat_construct ({txt=Lident "()"},_,[])}; c_rhs}] -> process_expr (api,heap) c_rhs
+    | _ -> assert false
+  end
   | _ -> failwith ("not implemented " ^ UntypeP.expr e)
 
 
@@ -159,6 +164,7 @@ let work { Misc.sourcefile = filename } (t: Typedtree.structure) =
   Format.pp_set_margin Format.std_formatter 100;
   Format.printf "Processing implementation file '%s'\n%!" filename;
   Printtyped.implementation Format.std_formatter t;
-  let h = process_str standart_heap t in
+  let api,h = process_str (Heap.Api.empty) t in
   Sexplib.Sexp.output_hum_indent 2 stdout @@ Heap.sexp_of_t h;
+  Sexplib.Sexp.output_hum_indent 2 stdout @@ Heap.sexp_of_api (fst api);
   ()
--- heap.ml
+++ heap.ml
@@ -1,7 +1,7 @@
 module MyIdent = struct
   type longident = [%import: Longident.t] [@@deriving sexp_of]
 
-  type t = Ident.t 
+  type t = Ident.t
   let to_string ident = Ident.unique_name ident
    (* Sexplib.Sexp.to_string_hum @@ sexp_of_t ident *)
   let sexp_of_t ident  = to_string ident |> Sexplib.Std.sexp_of_string
@@ -9,6 +9,7 @@ module MyIdent = struct
   let equal = Ident.equal
 end
 
+let pp_longident () lident = Longident.flatten lident |> String.concat ~sep:"."
 type term = LI of heap option * MyIdent.t
           | CInt of int
           | BinOp of op * term * term
@@ -16,12 +17,13 @@ type term = LI of heap option * MyIdent.t
           | Call of term * term
           | Union of (pf * term) list
           | Lambda of { lam_argname: MyIdent.t option
-                      ; lam_api   : api 
+                      ; lam_api   : api
                       ; lam_eff   : heap
                       ; lam_body  : term
                       ; lam_is_rec: bool
                       }
 and api = (MyIdent.t * term) list [@@deriving sexp_of]
+(* TODO: it should be path here *)
 and t = HAssoc of (MyIdent.t * term) list
         (* Heap should be a mapping from terms to terms (array access, for example)
          * but for fibonacci it doesn't matter
@@ -41,8 +43,9 @@ and heap = t [@@deriving sexp_of]
 let pp_term () t = Sexplib.Sexp.to_string @@ sexp_of_term t
 (** Term operations *)
 let call fexpr arg = Call (fexpr, arg)
-let lambda lam_argname lam_api lam_eff lam_body = Lambda { lam_argname; lam_api; lam_eff; lam_body }
-let li h longident = LI (h,longident)
+let lambda lam_is_rec lam_argname lam_api lam_eff lam_body =
+  Lambda { lam_argname; lam_api; lam_eff; lam_body; lam_is_rec }
+let li ?heap longident = LI (heap, longident)
 let union xs = Union xs
 let union2 g1 t1 g2 t2 = union [(g1,t1); (g2,t2)]
 let binop op a b = BinOp (op,a,b)
@@ -71,55 +74,66 @@ let rec hfind_exn (heap: heap) ident : term =
   match hfind heap ident with
   | None -> raise Not_found
   | Some ans -> ans
-and hfind heap ident = 
-  match heap with 
-  | HAssoc xs -> List.Assoc.find xs ~equal:Longident.equal ident 
-  | HEmpty -> None 
-  | HMerge hs -> 
+and hfind heap ident =
+  match heap with
+  | HAssoc xs -> List.Assoc.find xs ~equal:Longident.equal ident
+  | HEmpty -> None
+  | HMerge hs ->
       List.fold_right ~init:[] hs ~f:()
       union @@ List.map hs ~f:(fun (g,h) -> (g, hfind)
   | HCmps (_,_) -> assert false *)
-let rec hfind_li heap longident =
+let rec hfind_li (heap: heap) longident : term =
   (* let (>>=) = Option.(>>=) in  *)
-  let default = li heap longident in 
-  match heap with 
+  let default = li ~heap longident in
+  match heap with
   | HAssoc xs -> List.Assoc.find xs ~equal:MyIdent.equal longident
                  |> Option.value_map ~f:(fun t -> t) ~default
   | HEmpty -> default
-  | HMerge hs ->      
+  | HMerge hs ->
       union @@ List.map hs ~f:(fun (g,h) -> (g, hfind_li h longident))
-  | HCmps (a,b) -> 
+  | HCmps (a,b) ->
       fat_dot a (hfind_li b longident)
-  
+  | HCall (_,_) -> default
 
-and fat_dot heap term = 
-  match term with 
-  | Unit -> term 
+and fat_dot (heap: t) term =
+  match term with
+  | Unit -> term
   | CInt _ -> term
-  | LI (heap, longident) -> hfind_li heap longident
+  | LI (None, longident) -> hfind_li heap longident
+  | LI (Some _h, longident) ->
+      Format.eprintf "I don't know what to do in this case %s %d\n%!" __FILE__ __LINE__;
+      hfind_li heap longident
   | BinOp (op,l,r) -> BinOp (op, fat_dot heap l, fat_dot heap r)
   | Call (f, arg) -> Call (fat_dot heap f, fat_dot heap arg)
   | Union xs -> union @@ List.map xs ~f:(fun (pf,t) -> (fat_dot_pf heap pf, fat_dot heap t))
-  | Lambda  { lam_argname; lam_eff; lam_body } -> 
-      let () = Format.eprintf "Probably not implemented in %s %d" __FILE__ __LINE__ in 
+  | Lambda  { lam_argname; lam_eff; lam_body } ->
+      let () = Format.eprintf "Probably not implemented in %s %d" __FILE__ __LINE__ in
       term
 and simplify_term term = term
-and fat_dot_pf heap pf = match pf with 
+and fat_dot_pf heap pf = match pf with
   | LogicBinOp (op, l, r) -> LogicBinOp (op, fat_dot_pf heap l, fat_dot_pf heap r)
   | Not pf -> Not (fat_dot_pf heap pf)
   | Term t -> Term (fat_dot heap t)
 and simplify_pf pf = pf
 
-let rec heap_subst heap lident new_term = heap 
+let rec heap_subst heap lident new_term = heap
 and term_subst term lident new_term = term
 
-module Api = struct 
-  type t = api * MyIdent.t list 
+module Api = struct
+  type t = api * MyIdent.t list
+  let empty = ([],[])
   let find_exn ((xs,_):t) k = List.Assoc.find_exn xs k ~equal:MyIdent.equal
-  let add (api,pen) k v = 
+  let add (api,pen) k v =
     (List.Assoc.add ~equal:MyIdent.equal api k v, pen)
 
-  let add_pending new_ (api,xs) = (api,new_::xs)
+  let add_pending (api,xs) new_ = (api,new_::xs)
 
-  let remove_pending_exn el (api,xs) = (api, List.filter xs ~f:(MyIdent.equal el))
-end 
+  let remove_pending_exn (api,xs) el = (api, List.filter xs ~f:(MyIdent.equal el))
+
+  let is_pending (_,xs) ident = Base.List.exists xs ident
+  let is_pending_lident (_,xs) (lident: Longident.t) : MyIdent.t option =
+    assert false
+  let find_lident_exn : t -> Longident.t -> term = fun (api,toplevel) lident ->
+    assert false
+  let get_ident : t -> Longident.t -> MyIdent.t = fun  _ _ -> assert false
+end
