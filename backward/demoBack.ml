4a837b6c6ea7bc2baaa6a111abde418175309064
kakadu <kakadu@pm.me>
2021-06-18 21:15:25 +0300

    WIP on backward demo

--- backward/demoBack.ml
+++ backward/demoBack.ml
@@ -1,59 +1,513 @@
-type program
-type loc
-type loc_set
-type level_t = int
-type formula
-type formula_set
-type pobs_set
-type store_t
-type state_set
-type pob_state_set
+open Base
+open Ppx_compare_lib.Builtin
 
-module PobMap = struct type _ t end
-module LocMap = struct type _ t end
-module LocLevelMap = struct type _ t end
-type pob = { pob_loc: loc; phi: formula; pob_lvl: level_t }
-type state =
-{ st_loc : loc;
-  pc : formula ;
-  store: store_t;
-  loc0: formula;
-  st_lvl: level_t;
-  pobs : pobs_set;
-}
+module Loc = struct
+  type t = int [@@deriving compare]
+
+  module X = struct
+    type nonrec t = t
+
+    let compare = compare
+  end
+
+  module Set = Stdlib.Set.Make (X)
+
+  module Map = struct
+    include Stdlib.Map.Make (X)
+
+    let find_exn k m = find k m
+    let replace k v s = add k v s
+  end
+
+  let eq : t -> t -> bool = Int.equal
+  let replace k v m = Map.add k v m
+end
+
+type loc_set = Loc.Set.t
+type inf_level
+type ground_level
+
+type 'a level_t =
+  | LvlInt : int -> ground_level level_t
+  | LvlInf : inf_level level_t
+
+let compare_level_t :
+      'a 'b. ('a -> 'b -> int) -> 'a level_t -> 'b level_t -> int =
+  fun (type a b) _ (a : a level_t) (b : b level_t) : int ->
+   match (a, b) with
+   | LvlInf, LvlInf -> 0
+   | LvlInt _, LvlInf -> 1
+   | LvlInf, LvlInt _ -> -1
+   | LvlInt l, LvlInt r -> compare_int l r
+
+let my_compare_level a b = compare_level_t (fun _ _ -> assert false) a b
+
+module Term = struct
+  type t = Symb of string | ConcVar of string | Add of t * t | Int of int
+  [@@deriving compare]
+
+  let symb s = Symb s
+  let var s = ConcVar s
+  let add l r = Add (l, r)
+  let ( + ) = add
+  let int n = Int n
+end
+
+module Formula = struct
+  type t =
+    | True
+    | False
+    | Conj of t * t
+    | Disj of t * t
+    | LT of Term.t * Term.t
+    | EQ of Term.t * Term.t
+    | LE of Term.t * Term.t
+    | Term of Term.t
+  [@@deriving compare]
+
+  let term t = Term t
+  let le a b = LE (a, b)
+  let ( <= ) = le
+  let eq a b = EQ (a, b)
+  let ( = ) = eq
+  let conj a b = Conj (a, b)
+  let ( && ) = conj
+
+  module Set = Stdlib.Set.Make (struct
+    type nonrec t = t
+
+    let compare = compare
+  end)
+end
+
+module Store = struct
+  module StringMap = Stdlib.Map.Make (String)
+
+  type t = Formula.t StringMap.t
+  (* TODO: formula or term? *) [@@deriving compare]
+
+  let add = StringMap.add
+  let empty = StringMap.empty
+end
+
+module POB = struct
+  (* FIXME: GADT indexes fuck up generic programming
+     TODO: maybe annotate parameters-as-indexes and generate more smart comparison functions ? *)
+  type 'a t = {pob_loc: Loc.t; phi: Formula.t; pob_lvl: 'a level_t}
+  [@@deriving compare]
+
+  let my_compare : 'a 'b. 'a t -> 'b t -> int =
+   fun {pob_loc; phi; pob_lvl} {pob_loc= pob_loc2; phi= xi; pob_lvl= pob_lvl2} ->
+    match Loc.compare pob_loc pob_loc2 with
+    | 0 -> (
+      match Formula.compare phi xi with
+      | 0 -> compare_level_t (fun _ _ -> assert false) pob_lvl pob_lvl2
+      | n -> n )
+    | n -> n
+
+  let loc {pob_loc} = pob_loc
+  let level : ground_level t -> int = function {pob_lvl= LvlInt n} -> n
+  let formula {phi} = phi
+  let create pob_loc phi pob_lvl = {pob_loc; phi; pob_lvl}
+
+  type 'a pob = 'a t
+
+  module X = struct
+    type gt = POB : 'a t -> gt
+    type t = gt
+
+    include Comparator.Make (struct
+      type t = gt
+
+      let compare a b = match (a, b) with POB l, POB r -> my_compare l r
+      let sexp_of_t = function _ -> assert false
+    end)
+  end
+
+  module Set = struct
+    type key = X.gt
+    type 'a pob = 'a t
+    type t = (key, X.comparator_witness) Set.t
+
+    let cmp :
+        (module Comparator.S
+           with type comparator_witness = X.comparator_witness
+            and type t = X.t ) =
+      (module X)
+
+    let empty : t = Set.empty cmp
+    let add_exn : 'a pob -> t -> t = fun k s -> Set.add s (X.POB k)
+
+    let compare : t -> t -> int =
+     (* TODO: Ask why I should write this shit *)
+     fun l r -> Base.Set.compare_m__t (module struct end) l r
+
+    let iter : f:(key -> unit) -> t -> unit =
+     fun ~f set -> Base.Set.iter set ~f:(fun k -> f k)
+  end
+
+  module Map = struct
+    type 'a t = (X.t, 'a, X.comparator_witness) Map.t
+
+    let cmp = Set.cmp
+    let empty : _ t = Map.empty cmp
+
+    let compare : ('a -> 'a -> int) -> 'a t -> 'a t -> int =
+     (* TODO: Ask why I should write this shit *)
+     fun onval l r -> Base.Map.compare_m__t (module struct end) onval l r
+
+    let find_exn : 'a pob -> 'b t -> 'b =
+     fun k m -> Base.Map.find_exn m (X.POB k)
+  end
+end
+
+(** Symbolic state *)
+module State = struct
+  (* TODO: say in the interface that it is private *)
+  type t =
+    { idx: int
+    ; loc: Loc.t
+    ; pc: Formula.t
+    ; store: Store.t
+    ; loc0: Loc.t
+    ; lvl: ground_level level_t
+    ; pobs: POB.Set.t }
+
+  let my_compare {idx} {idx= idx2} = compare_int idx idx2
+  let index {idx} = idx
+  let loc {loc} = loc
+  let pc {pc} = pc
+  let loc0 {loc0} = loc0
+  let pobs {pobs} = pobs
+  let level : t -> int = function {lvl= LvlInt n} -> n
+  let last = ref 0
+
+  type state = t
+
+  let create ~loc ~loc0 store pc lvl pobs =
+    incr last ;
+    {idx= !last; loc; loc0; pc; store; lvl; pobs}
+
+  module X = struct
+    type nonrec t = t
+
+    include Comparator.Make (struct
+      type nonrec t = t
+
+      let compare = my_compare
+      let sexp_of_t = function _ -> assert false
+    end)
+  end
+
+  module Set = struct
+    type t = (X.t, X.comparator_witness) Set.t
+
+    let cmp :
+        (module Comparator.S
+           with type comparator_witness = X.comparator_witness
+            and type t = X.t ) =
+      (module X)
+
+    let empty : t = Set.empty cmp
+    let singleton x : t = Set.singleton cmp x
+
+    let compare : t -> t -> int =
+     (* TODO: Ask why I should write this shit *)
+     fun l r -> Base.Set.compare_m__t (module struct end) l r
+
+    let iter : t -> f:(_ -> unit) -> _ = Base.Set.iter
+    let fold : t -> _ = Base.Set.fold
+    let add k s : t = Base.Set.add s k
+    let remove k set = Base.Set.remove set k
+  end
+end
+
+module PobStateSet = struct
+  module X = struct
+    type t = P : 'a POB.t * State.t -> t
+
+    include Comparator.Make (struct
+      type nonrec t = t
+
+      let compare (P (a, c)) (P (b, d)) =
+        match POB.my_compare a b with 0 -> State.my_compare c d | n -> n
+
+      let sexp_of_t = function _ -> assert false
+    end)
+  end
+
+  type 'a key = 'a POB.t * State.t
+  type t = (X.t, X.comparator_witness) Set.t
+
+  let cmp :
+      (module Comparator.S
+         with type comparator_witness = X.comparator_witness
+          and type t = X.t ) =
+    (module X)
+
+  let empty : t = Set.empty cmp
+
+  let compare : t -> t -> int =
+   (* TODO: Ask why I should write this shit *)
+   fun l r -> Base.Set.compare_m__t (module struct end) l r
+
+  let remove : _ key -> t -> t = fun (p, st) s -> Set.remove s (X.P (p, st))
+  let add : _ key -> t -> t = fun (p, st) s -> Set.add s (X.P (p, st))
+end
+
+module Program = struct
+  type t
+
+  let ep _ = assert false
+end
+
+type program = Program.t
+type state = State.t
+type 'a pob = 'a POB.t
+type state_set = State.Set.t
+type pob_state_set = PobStateSet.t
+type pobs_set = POB.Set.t
+
+module LocLevelMap = struct
+  module Key = struct
+    type t = X : Loc.t * 'a level_t -> t
+
+    let compare (X (a, c)) (X (b, d)) =
+      match Loc.compare a b with 0 -> my_compare_level c d | n -> n
+  end
+
+  include Stdlib.Map.Make (Key)
+
+  let add : Loc.t * _ level_t -> 'v -> 'v t -> 'v t =
+   fun (l, l2) v m -> add (Key.X (l, l2)) v m
+
+  let add_ground_lvl (loc, lvln) = add (loc, LvlInt lvln)
+end
 
 type globals =
-{ mutable curLvl: level_t
-; mutable main_pobs: pobs_set
-; mutable pobs : pobs_set
-; mutable qf: state_set
-; mutable qb: pob_state_set
-; mutable witnesses: state_set PobMap.t
-; mutable blocked_locs: loc_set PobMap.t
-; mutable pobs_locs: loc_set
-; mutable t : state_set LocMap.t
-; mutable l : formula_set LocLevelMap.t
-; mutable child: pobs_set PobMap.t
-}
+  { mutable curLvl: ground_level level_t
+  ; mutable main_pobs: pobs_set
+  ; mutable pobs: pobs_set
+  ; mutable qf: state_set
+  ; mutable qb: pob_state_set
+  ; mutable witnesses: state_set POB.Map.t
+  ; mutable blocked_locs: loc_set POB.Map.t
+  ; mutable pobs_locs: loc_set
+  ; mutable t: state_set Loc.Map.t
+  ; mutable l: Formula.Set.t LocLevelMap.t
+  ; mutable child: pobs_set POB.Map.t }
+
+type strategy_t = {chooseAction: unit -> unit}
 
 module type IFACE = sig
   val propDirSymExec : loc_set -> program -> unit
   val forward : state -> unit
-  val backward : pob -> state -> program -> level_t
-  val answerYes: pob -> unit
-  val answerNo : pob -> unit
 
-  val canReach: loc -> loc -> loc_set -> bool
+  val backward :
+    ground_level pob -> state -> program -> ground_level level_t -> unit
 
-  val checkInductive : level_t -> unit
+  val checkInductive : 'a level_t -> unit
+  val addWitness : state -> 'a pob -> unit
+  val blockWitness : state -> 'a pob -> unit
 
-  val addWitness : state -> pob -> unit
+  val overApxm :
+       Loc.t
+    -> lvl:ground_level level_t
+    -> cur_lvl:ground_level level_t
+    -> Formula.t
 
-  val blockWitness: state -> pob -> unit
+  val encode : state -> Formula.t
+  val wlp : state -> Formula.t -> Formula.t
 
-  val overApxm: loc -> lvl:level_t -> cur_lvl:level_t -> formula
+  (** External functions *)
 
-  val encode: state -> formula
+  val isSat : Formula.t -> bool
+  val answerYes : 'a pob -> unit
+  val answerNo : 'a pob -> unit
+  val nextLevel : ground_level level_t -> unit
+  val canReach : Loc.t -> dest:Loc.t -> loc_set -> bool
+  val executeInstruction : state -> Loc.t -> state_set
+  val mkPrime : Formula.t -> Formula.t
+end
 
-  val wlp : state -> formula -> formula
+module Hardcoded = struct
+  module State = struct
+    let pM : _ POB.t = POB.create 7 True LvlInf
+
+    let s1 : state =
+      State.create ~loc0:2 ~loc:3
+        Store.(
+          empty
+          |> add "x" (Formula.term @@ Term.int 1)
+          |> add "y" (Formula.term @@ Term.int 1))
+        Formula.True (LvlInt 0)
+        POB.Set.(add_exn pM empty)
+
+    let s2 : state =
+      State.create ~loc0:3 ~loc:5
+        Store.(
+          empty
+          |> add "x" (Formula.term @@ Term.(add (symb "x") (symb "y")))
+          |> add "y" (Formula.term @@ Term.symb "y"))
+        Formula.True (LvlInt 0)
+        POB.Set.(add_exn pM empty)
+
+    let s3 : state =
+      State.create ~loc0:5 ~loc:3
+        Store.(
+          empty
+          |> add "x" (Formula.term @@ Term.(symb "x"))
+          |> add "y" (Formula.term @@ Term.symb "x"))
+        Formula.True (LvlInt 1)
+        POB.Set.(add_exn pM empty)
+
+    let s4 : state =
+      State.create ~loc0:3 ~loc:7
+        Store.(
+          empty
+          |> add "x" (Formula.term @@ Term.(symb "x"))
+          |> add "y" (Formula.term @@ Term.symb "x"))
+        Formula.(le (Term.symb "x") (Term.int 0))
+        (LvlInt 0)
+        POB.Set.(add_exn pM empty)
+  end
+end
+
+module G = struct
+  let add_child g ch ~parent = assert false
+  let add_to_qb g (p, st) = g.qb <- PobStateSet.add (p, st) g.qb
+  let add_to_qf g s = g.qf <- State.Set.add s g.qf
+  let remove_from_qf g s = g.qf <- State.Set.remove s g.qf
+  let add_to_l g loc lvl new_val = g.l <- LocLevelMap.add (loc, lvl) new_val g.l
+
+  let add_to_t g loc (st : state) =
+    let next_set =
+      try
+        let old = Loc.Map.find loc g.t in
+        State.Set.add st old
+      with Not_found_s _ -> State.Set.singleton st in
+    g.t <- Loc.Map.replace loc next_set g.t
+
+  let blocked_locs g _ = assert false
+  let t p' = assert false
+end
+
+module IMPL : IFACE = struct
+  let init_globals () =
+    { curLvl= LvlInt 0
+    ; main_pobs= POB.Set.empty
+    ; pobs= POB.Set.empty
+    ; qf= State.Set.empty
+    ; qb= PobStateSet.empty
+    ; witnesses= POB.Map.empty
+    ; blocked_locs= POB.Map.empty
+    ; pobs_locs= Loc.Set.empty
+    ; t= Loc.Map.empty
+    ; l= LocLevelMap.empty
+    ; child= POB.Map.empty }
+
+  let g : globals = init_globals ()
+  let addWitness _ _ = assert false
+  let blockWitness _ _ = assert false
+  let wlp _ _ = assert false
+  let isSat _ = assert false
+  let isUnsat _ = assert false
+  let answerYes _ = assert false
+  let answerNo _ = assert false
+  let overApxm _ ~lvl:_ ~cur_lvl:_ = assert false
+  let nextLevel _ = assert false
+  let canReach _ ~dest:_ _ = assert false
+  let executeInstruction _ _ = assert false
+  let mkPrime _ = assert false
+  let checkInductive _ = assert false
+  let generalize _ _ = assert false
+
+  let encode : state -> Formula.t =
+    (* Тут можно генерировать формулки для Z3, так как по ним мэтчиться не придется
+       Но там надо будет вместо штрихов придумывать новые имена
+    *)
+    let warned = ref false in
+    fun st ->
+      let () =
+        if not !warned then (
+          warned := true ;
+          Format.eprintf "Hardcoded `encode` implementation\n%!" ) in
+      let open Formula in
+      let x = Term.symb "x" in
+      let x' = x in
+      let y = Term.symb "y" in
+      let y' = y in
+      let _1 = Term.int 1 in
+      let _0 = Term.int 0 in
+      match State.index st with
+      | 1 -> x' = _1 && y' = _1
+      | 2 -> (x' = Term.(x + y)) && y' = y
+      | 3 -> x' = x && y' = x
+      | 4 -> x' <= _0 && x' = x && y' = y
+      | _ -> failwith "Should not happen in demo example"
+
+  let backward p' s' prog cur_lvl =
+    g.qb <- PobStateSet.(remove (p', s') g.qb) ;
+    assert (Loc.eq (POB.loc p') (State.loc s')) ;
+    let lvl = POB.level p' - State.level s' in
+    let () = assert (lvl >= 0) in
+    let psi =
+      Formula.conj
+        (wlp s' (POB.formula p'))
+        (overApxm (State.loc0 s') ~lvl ~cur_lvl:g.curLvl) in
+    ( if isSat psi then (
+      if POB.loc p' = Program.ep prog then answerYes p'
+      else
+        let p = POB.create (State.loc0 s') psi (LvlInt lvl) in
+        let () = g.pobs <- POB.Set.add_exn p g.pobs in
+        let () = G.add_child g ~parent:p' p in
+        State.Set.iter (POB.Map.find_exn p' g.witnesses) ~f:(fun s ->
+            addWitness s p ) ;
+        State.Set.iter
+          (Loc.Map.find_exn (POB.loc p) g.t)
+          ~f:(fun s -> G.add_to_qb g (p, s)) )
+    else
+      let () = blockWitness s' p' in
+      if canReach (Program.ep prog) ~dest:(POB.loc p') (G.blocked_locs g p')
+      then () (*return *)
+      else
+        let () = answerNo p' in
+        let apxm =
+          State.Set.fold
+            (G.t (POB.loc p'))
+            ~init:Formula.False
+            ~f:(fun acc s ->
+              if Loc.Set.mem (State.loc0 s) (G.blocked_locs g p') then
+                Formula.(
+                  acc
+                  && overApxm (State.loc0 s)
+                       ~lvl:(POB.level p' - State.level s)
+                       ~cur_lvl
+                  && encode s)
+              else acc ) in
+        let () =
+          G.add_to_l g (POB.loc p')
+            (LvlInt (POB.level p'))
+            (generalize apxm (POB.formula p')) in
+        checkInductive cur_lvl ) ;
+    ()
+
+  let forward s =
+    G.remove_from_qf g s ;
+    State.Set.iter
+      (executeInstruction s (State.loc s))
+      ~f:(fun s' ->
+        assert (isSat (State.pc s')) ;
+        G.add_to_qf g s' ;
+        if Loc.Set.mem (State.loc s') g.pobs_locs then
+          G.add_to_t g (State.loc s') s' ;
+        POB.Set.iter (State.pobs s) ~f:(fun (POB.X.POB p) ->
+            addWitness s' p ;
+            if POB.loc p = State.loc s' then G.add_to_qb g (p, s') ) ) ;
+    POB.Set.iter (State.pobs s) ~f:(fun (POB.X.POB p) ->
+        if POB.loc p <> State.loc s then blockWitness s p )
+
+  let propDirSymExec locs prog =
+    (* init_globals () ; *)
+    assert false
 end
